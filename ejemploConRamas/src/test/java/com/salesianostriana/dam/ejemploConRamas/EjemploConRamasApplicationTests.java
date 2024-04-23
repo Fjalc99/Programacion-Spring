23e49db177d4c8c8cb8d7f7fca2d0c49807804f0
Fran <alcantarilla.cafra24@triana.salesianos.edu>
2024-04-23 09:03:38 +0200

    proyecto

---
+++ ejemploConRamas/src/test/java/com/salesianostriana/dam/ejemploConRamas/EjemploConRamasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.ejemploConRamas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploConRamasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
