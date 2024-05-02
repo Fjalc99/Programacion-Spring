711d2f5dc29b57b2ab47d353e6ecae272e002661
Francisco José Alcantarilla Calado <alcantarilla.cafra24@triana.salesianos.edu>
2024-05-02 20:50:09 +0200

    Proyecto

--- FranciscoJoseAlcantarillaCaladoProyecto-2/src/main/java/com/salesianostriana/dam/FranciscoJoseAlcantarillaCaladoProyecto2/controller/ProductoController.java
+++ FranciscoJoseAlcantarillaCaladoProyecto-2/src/main/java/com/salesianostriana/dam/FranciscoJoseAlcantarillaCaladoProyecto2/controller/ProductoController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.salesianostriana.dam.FranciscoJoseAlcantarillaCaladoProyecto2.model.Producto;
 import com.salesianostriana.dam.FranciscoJoseAlcantarillaCaladoProyecto2.service.ProductoService;
 
 @Controller
@@ -24,5 +27,26 @@ public class ProductoController {
 	private String vistaTienda(Model model) {
 		model.addAttribute("listaProductos", productoservice.findAll());
 		return "producto";
+	}
+	
+	
+	/*@GetMapping("/descripcionProducto")
+	private String vistaDescripcion(Model model) {
+		Producto producto = new Producto ();
+		model.addAttribute("vistaDescripcion", producto);
+		return "descripcionDelProducto";
+	}*/
+	
+	@GetMapping("/formularioProducto")
+	public String formularioProducto(Model model) {
+		Producto producto = new Producto ();
+		model.addAttribute("productoForm", producto);
+		
+		return "formularioProductos";
+	}
+	
+	@PostMapping("/addProducto")
+	public String submit(@ModelAttribute("productoForm")Producto producto, Model model) {
+		return "productosVistaAdmin";
 	}
 }
